71d72ffd9115f8c2d8a9a4ffdff3722d41d6466d
RomanPas <unlike05@gmail.com>
2020-06-13 13:56:25 +0300

    fix2

--- main.cpp
+++ main.cpp
@@ -17,16 +17,15 @@ int main()
 	string myString("string");
 	vector<int> myVector = { 0, 1, 2, 3 };
 	list<int> myList = { 4, 5, 6, 7 };
-	tuple<int, int, int, int> myTuple = { 8,9,10,11 };
-	//- Опционально.Печать адреса как std::tuple
+	tuple<int, int, int, int> myTuple = { 8, 9, 10, 11 };
 
-	//print_ip(myChar);
-	//print_ip(meShort);
-	//print_ip(myInt);
-	//print_ip(myLong);
-	//print_ip(myString);
-	//print_ip(myVector);
-	//print_ip(myList);
+	print_ip(myChar);
+	print_ip(meShort);
+	print_ip(myInt);
+	print_ip(myLong);
+	print_ip(myString);
+	print_ip(myVector);
+	print_ip(myList);
 	print_ip(myTuple);
 
 	return 0;
